d10a2c4c3e54cea8b76d6aa657f2bc9609810d46
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-10-09 18:10:42 -0300

    remove tag label and attr unless

--- client/src/components/buttonChange/index.tsx
+++ client/src/components/buttonChange/index.tsx
@@ -7,10 +7,10 @@ const ButtonChange = () => {
 	
   return(
   <>
-		<div  className="w-12 h-6 pr-6 rounded-full bg-gray-300 m-24" onClick={HandleClick}>
-				<label id="ButtonChange" className={`w-6 h-1/1 block rounded-full bg-gray-400 transform mx-${move} duration-300 top-0 `} htmlFor="ButtonChange" onClick={HandleClick}/>
+		<div  className={`w-12 h-6 pr-6 rounded-full bg-gray-300 m-24`} onClick={HandleClick}>
+				<div className={`w-6 h-1/1 block rounded-full bg-gray-400 transform mx-${move} duration-300 top-0 `} onClick={HandleClick}/>
 		</div>
-    </>
+	</>
   )
 }
 export default ButtonChange;
