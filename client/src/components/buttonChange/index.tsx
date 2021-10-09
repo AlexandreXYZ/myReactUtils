38ca5932a7158806a5ba7fd5b1aa34d64005924a
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-10-09 15:56:31 -0300

    add 'for' in container track Added my htmlFor in container the track

--- client/src/components/buttonChange/index.tsx
+++ client/src/components/buttonChange/index.tsx
@@ -3,10 +3,13 @@ import './style.css';
 
 const ButtonChange = () => {
   const [move, setMove] = useState('0');
+	const HandleClick = () => {
+		return setMove(move === '1/1' ? '0' : '1/1')
+	}
   return(
   <>
-		<div id="ButtonChange" className="w-12 h-6 pr-6 rounded-full bg-gray-300 m-24">
-				<div className={`w-6 h-1/1 rounded-full bg-gray-400 transform mx-${move} duration-300 top-0 `} onClick={ () => setMove(move === '1/1' ? '0' : '1/1')}/>
+		<div  className="w-12 h-6 pr-6 rounded-full bg-gray-300 m-24" onClick={HandleClick}>
+				<label id="ButtonChange" className={`w-6 h-1/1 block rounded-full bg-gray-400 transform mx-${move} duration-300 top-0 `} htmlFor="ButtonChange" onClick={HandleClick}/>
 		</div>
     </>
   )
