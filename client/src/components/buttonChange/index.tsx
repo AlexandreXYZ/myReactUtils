9e58ee1d72503c057599c58b53ead70120d5107d
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-10-09 17:33:28 -0300

    Refactor arrow function HandleClick

--- client/src/components/buttonChange/index.tsx
+++ client/src/components/buttonChange/index.tsx
@@ -3,9 +3,8 @@ import './style.css';
 
 const ButtonChange = () => {
   const [move, setMove] = useState('0');
-	const HandleClick = () => {
-		return setMove(move === '1/1' ? '0' : '1/1')
-	}
+	const HandleClick = () => (setMove(move === '1/1' ? '0' : '1/1'))
+	
   return(
   <>
 		<div  className="w-12 h-6 pr-6 rounded-full bg-gray-300 m-24" onClick={HandleClick}>
