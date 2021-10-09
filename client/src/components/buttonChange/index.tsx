f9258be8ea53ce1b2561489836c7643541b0a7d0
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-10-09 15:53:22 -0300

    Add button and button animation

--- client/src/components/buttonChange/index.tsx
+++ client/src/components/buttonChange/index.tsx
@@ -1,8 +1,13 @@
-// import './style.scss';
+import { useState } from 'react';
+import './style.css';
 
 const ButtonChange = () => {
+  const [move, setMove] = useState('0');
   return(
-    <>
+  <>
+		<div id="ButtonChange" className="w-12 h-6 pr-6 rounded-full bg-gray-300 m-24">
+				<div className={`w-6 h-1/1 rounded-full bg-gray-400 transform mx-${move} duration-300 top-0 `} onClick={ () => setMove(move === '1/1' ? '0' : '1/1')}/>
+		</div>
     </>
   )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+          '1/1': "100%",
+          '1/2': "50%",
+          '1/4': "25%",
+      }
+    },
   },
   variants: {
     extend: {},
