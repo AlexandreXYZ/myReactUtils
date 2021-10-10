3026993cb9bf2c78bc04fb49d75c1bcbf09d1c3c
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-10-10 14:53:33 -0300

    rename id in ButtonChange

--- client/src/components/buttonChange/index.tsx
+++ client/src/components/buttonChange/index.tsx
@@ -15,8 +15,8 @@ const ButtonChange = ( {width, height, onClick}: props ) => {
 	}
   return(
 		<>
-		<div id="test" className={`w-${width??12} h-${height??6} pr-${height} delay-1000 rounded-full bg-gray-300 m-24`} onClick={HandleClick}>
-				<label className={`w-${height??6} h-${height??6} block rounded-full bg-gray-400 transform mx-${move} duration-300 top-0 `} htmlFor="test"/>
+		<div id="ButtonChange" className={`w-${width??12} h-${height??6} pr-${height} delay-1000 rounded-full bg-gray-300 m-24`} onClick={HandleClick}>
+				<label className={`w-${height??6} h-${height??6} block rounded-full bg-gray-400 transform mx-${move} duration-300 top-0 `} htmlFor="ButtonChange"/>
 		</div>
 		</>
   )
