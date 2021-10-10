6e580311538e0f2cdf72bb13393c820a3b81103d
Alexandre Costa Belettini <costaalexandre45@gmail.com>
2021-10-10 12:30:22 -0300

    add props width, height and click

--- client/src/components/buttonChange/index.tsx
+++ client/src/components/buttonChange/index.tsx
@@ -1,16 +1,24 @@
 import { useState } from 'react';
 import './style.css';
 
-const ButtonChange = () => {
+interface props{
+	width?: string,
+	height?: string,
+	Click: Function,
+}
+
+const ButtonChange = ( {width, height, Click}: props ) => {
   const [move, setMove] = useState('0');
-	const HandleClick = () => (setMove(move === '1/1' ? '0' : '1/1'))
-	
+	const HandleClick = () => {
+		setMove(move === '1/1' ? '0' : '1/1');
+		Click();
+	}
   return(
-  <>
-		<div  className={`w-12 h-6 pr-6 rounded-full bg-gray-300 m-24`} onClick={HandleClick}>
-				<div className={`w-6 h-1/1 block rounded-full bg-gray-400 transform mx-${move} duration-300 top-0 `} onClick={HandleClick}/>
+		<>
+		<div className={`w-${width??12} h-${height??6} pr-${height} delay-1000 rounded-full bg-gray-300 m-24`} onClick={HandleClick}>
+				<div className={`w-${height??6} h-${height??6} rounded-full bg-gray-400 transform mx-${move} duration-300 top-0 `} onClick={HandleClick}/>
 		</div>
-	</>
+		</>
   )
 }
 export default ButtonChange;
